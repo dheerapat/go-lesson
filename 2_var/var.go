61c1245feb9aae25af0c1c81d50fdebb67f88cda
Dheerapat Tookkane <dheerapat.t@kkumail.com>
2023-03-19 13:43:31 +0700

    go lesson

---
+++ 2_var/var.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		player        = 3
+		replay        = false
+		namePlayerOne = "Chris"
+	)
+
+	fmt.Println(player)
+	fmt.Println(replay)
+	fmt.Println(namePlayerOne)
+}
